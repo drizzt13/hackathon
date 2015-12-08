cb72b23d8153c1972c5f245b722b98d2e1b05011
Panos Zhu <PanosZhu@Gmail.com>
2015-12-07 16:23:18 -0800

    delete compute bearing function

--- src/main/java/com/expedia/derbysoft/hackathon/utils/latlng/LatLng.java
+++ src/main/java/com/expedia/derbysoft/hackathon/utils/latlng/LatLng.java
@@ -28,14 +28,6 @@ public class LatLng {
         return computeDistance(this.getLat(), this.getLng(), dest.getLat(), dest.getLng());
     }
 
-    public int bearingTo(LatLng dest) {
-        int azimuth = (int) Math.round(computeBearing(lat, lng, dest.getLat(), dest.getLng()));
-        if (azimuth < 0) {
-            azimuth += 360;
-        }
-        return azimuth;
-    }
-
     private static double computeDistance(BigDecimal lat1, BigDecimal lng1, BigDecimal lat2, BigDecimal lng2) {
         if (lat1 == null || lng1 == null || lat2 == null || lng2 == null) {
             return -1;
@@ -62,70 +54,6 @@ public class LatLng {
 
     private static double rad(double d) {
         return d * Math.PI / DOUBLE;
-    }
-
-    private static double computeBearing(BigDecimal lat1, BigDecimal lng1, BigDecimal lat2, BigDecimal lng2) {
-        if (lat1 == null || lng1 == null || lat2 == null || lng2 == null) {
-            return -99999;
-        }
-        return computeBearing(lat1.doubleValue(), lng1.doubleValue(), lat2.doubleValue(), lng2.doubleValue());
-    }
-
-    private static double computeBearing(double lat1, double lng1, double lat2, double lng2) {
-        if (!validateLngLat(new LatLng(lat1, lng1)) || !validateLngLat(new LatLng(lat2, lng2))) {
-            return -99999;
-        }
-        lat1 *= Math.PI / 180.0;
-        lat2 *= Math.PI / 180.0;
-        lng1 *= Math.PI / 180.0;
-        lng2 *= Math.PI / 180.0;
-        double a = EARTH_RADIUS * 1000; // WGS84 major axis
-        double b = 6356752.3142; // WGS84 semi-major axis
-        double f = (a - b) / a;
-        double L = lng2 - lng1;
-        double U1 = Math.atan((1.0 - f) * Math.tan(lat1));
-        double U2 = Math.atan((1.0 - f) * Math.tan(lat2));
-
-        double cosU1 = Math.cos(U1);
-        double cosU2 = Math.cos(U2);
-        double sinU1 = Math.sin(U1);
-        double sinU2 = Math.sin(U2);
-        double cosU1cosU2 = cosU1 * cosU2;
-        double sinU1sinU2 = sinU1 * sinU2;
-
-        double sigma;
-        double cosSqAlpha;
-        double cos2SM;
-        double cosSigma;
-        double sinSigma;
-        double cosLambda = 0.0;
-        double sinLambda = 0.0;
-
-        double lambda = L;
-        int maxIters = 20;
-        for (int iter = 0; iter < maxIters; iter++) {
-            double lambdaOrig = lambda;
-            cosLambda = Math.cos(lambda);
-            sinLambda = Math.sin(lambda);
-            double t1 = cosU2 * sinLambda;
-            double t2 = cosU1 * sinU2 - sinU1 * cosU2 * cosLambda;
-            double sinSqSigma = t1 * t1 + t2 * t2;
-            sinSigma = Math.sqrt(sinSqSigma);
-            cosSigma = sinU1sinU2 + cosU1cosU2 * cosLambda;
-            sigma = Math.atan2(sinSigma, cosSigma);
-            double sinAlpha = (sinSigma == 0) ? 0.0 : cosU1cosU2 * sinLambda / sinSigma;
-            cosSqAlpha = 1.0 - sinAlpha * sinAlpha;
-            cos2SM = (cosSqAlpha == 0) ? 0.0 : cosSigma - 2.0 * sinU1sinU2 / cosSqAlpha;
-            double c = (f / 16.0) * cosSqAlpha * (4.0 + f * (4.0 - 3.0 * cosSqAlpha));
-            lambda = L + (1.0 - c) * f * sinAlpha * (sigma + c * sinSigma * (cos2SM + c * cosSigma * (-1.0 + 2.0 * cos2SM * cos2SM)));
-            double delta = (lambda - lambdaOrig) / lambda;
-            if (Math.abs(delta) < 1.0e-12) {
-                break;
-            }
-        }
-        double initialBearing = Math.atan2(cosU2 * sinLambda, cosU1 * sinU2 - sinU1 * cosU2 * cosLambda);
-        initialBearing *= 180.0 / Math.PI;
-        return initialBearing;
     }
 
     public BigDecimal getLng() {
--- src/main/java/com/expedia/derbysoft/hackathon/utils/latlng/LatLngs.java
+++ src/main/java/com/expedia/derbysoft/hackathon/utils/latlng/LatLngs.java
@@ -21,16 +21,4 @@ public abstract class LatLngs {
         return new LatLng(lat1, lng1).distanceTo(new LatLng(lat2, lng2));
     }
 
-    public static int azimuth(LatLng latLng1, LatLng latLng2) {
-        return azimuth(latLng1.getLat(), latLng1.getLng(), latLng2.getLat(), latLng2.getLng());
-    }
-
-    public static int azimuth(BigDecimal lat1, BigDecimal lng1, BigDecimal lat2, BigDecimal lng2) {
-        return azimuth(lat1.doubleValue(), lng1.doubleValue(), lat2.doubleValue(), lng2.doubleValue());
-    }
-
-    public static int azimuth(double lat1, double lng1, double lat2, double lng2) {
-        return new LatLng(lat1, lng1).bearingTo(new LatLng(lat2, lng2));
-    }
-
 }
