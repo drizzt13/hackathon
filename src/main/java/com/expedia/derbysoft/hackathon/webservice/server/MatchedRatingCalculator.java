9440773d0d3de9567d5755b43890910b9864d1cd
Panos Zhu <PanosZhu@Gmail.com>
2015-12-07 17:13:38 -0800

    r

--- src/main/java/com/expedia/derbysoft/hackathon/webservice/server/MatchedRatingCalculator.java
+++ src/main/java/com/expedia/derbysoft/hackathon/webservice/server/MatchedRatingCalculator.java
@@ -22,36 +22,36 @@ public class MatchedRatingCalculator {
 
     public static BigDecimal calculateRatting(BigDecimal distance, BigDecimal guestRating) {
         if (distance.doubleValue() < 0.5) {
-            return guestRating.add(BigDecimal.valueOf(0.6));
-        }
-        if (distance.doubleValue() < 1) {
             return guestRating.add(BigDecimal.valueOf(0.5));
         }
-        if (distance.doubleValue() < 1.5) {
+        if (distance.doubleValue() < 1) {
             return guestRating.add(BigDecimal.valueOf(0.4));
         }
-        if (distance.doubleValue() < 2) {
+        if (distance.doubleValue() < 1.5) {
             return guestRating.add(BigDecimal.valueOf(0.3));
         }
-        if (distance.doubleValue() < 2.5) {
+        if (distance.doubleValue() < 2) {
             return guestRating.add(BigDecimal.valueOf(0.2));
         }
-        if (distance.doubleValue() < 3) {
+        if (distance.doubleValue() < 2.5) {
             return guestRating.add(BigDecimal.valueOf(0.1));
+        }
+        if (distance.doubleValue() < 3) {
+            return guestRating.add(BigDecimal.valueOf(0.05));
         }
         if (distance.doubleValue() < 4) {
             return guestRating;
         }
         if (distance.doubleValue() < 5) {
-            return guestRating.subtract(BigDecimal.valueOf(-0.2));
+            return guestRating.add(BigDecimal.valueOf(-0.2));
         }
         if (distance.doubleValue() < 10) {
-            return guestRating.subtract(BigDecimal.valueOf(-0.5));
+            return guestRating.add(BigDecimal.valueOf(-0.5));
         }
         if (distance.doubleValue() < 20) {
-            return guestRating.subtract(BigDecimal.valueOf(-1));
+            return guestRating.add(BigDecimal.valueOf(-1));
         }
-        return guestRating.subtract(BigDecimal.valueOf(-2));
+        return guestRating.add(BigDecimal.valueOf(-2));
     }
 
 }
